9fcba3ef89bd2123058d770c428932bf0de78500
Lucky <103184937+LuckySikolia@users.noreply.github.com>
2024-09-10 16:52:32 +0300

    Fix background moving on game over
    Fix repeat background to focus on x axis
    Fix  move left to check game over condition

--- Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -50,7 +50,7 @@ public class PlayerControllerX : MonoBehaviour
             explosionParticle.Play();
             playerAudio.PlayOneShot(explodeSound, 1.0f);
             gameOver = true;
-            Debug.Log("Game Over!");
+            Debug.Log("Game Over!");  
             Destroy(other.gameObject);
         } 
 
